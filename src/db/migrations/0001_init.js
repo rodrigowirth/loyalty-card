cd7bab3859b21282f5be980053b02278dc51b5a2
Rodrigo Wirth <rodrigo.wirth@bravi.com.br>
2017-07-02 10:17:29 -0300

    Fix migration

--- src/db/migrations/0001_init.js
+++ src/db/migrations/0001_init.js
@@ -1,11 +1,7 @@
 export function up (knex) {
   return knex
     .schema
-
-    .dropTable('stamps')
-    .dropTable('companies')
-    .dropTable('people')
-
+    
     .createTable('people', (table) => {
       table.increments('id').primary();
       table.string('guid').unique();
